88826cfafd6c001f14ce9f735583df71bff00421
Edilson do Nascimento <edilsonjoper@gmail.com>
2025-12-13 05:07:31 -0300

    st00 - chore: add initial project.

---
+++ src/main/java/br/ednascimento/taskmanager/TaskManagerCleanArchitectureApplication.java
@@ -0,0 +1,13 @@
+package br.ednascimento.taskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerCleanArchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagerCleanArchitectureApplication.class, args);
+	}
+
+}
